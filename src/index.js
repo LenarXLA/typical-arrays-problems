fdc5ade90815f0749329dfdc03e6bf77129fb44e
Lenar <leonruscraft@gmail.com>
2020-10-03 10:18:05 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    let minValue = array[0];
+    for (let i = 1; i < array.length; i++) {
+      let currentValue = array[i];
+      if(currentValue < minValue) {
+        minValue = currentValue;
+      }
+    }
+    return minValue;
+  } else {
+    return 0; 
+  } 
 }
-
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    let maxValue = array[0];
+    for(let i = 1; i < array.length; i++) {
+      let currentValue = array[i];
+      if(currentValue > maxValue) {
+        maxValue = currentValue;
+      }
+    }
+    return maxValue;
+  } else {
+    return 0; 
+  } 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    let result = 0;
+    for(let num in array) {
+      result += array[num];
+    }
+    return result / array.length;
+  } else {
+    return 0; 
+  } 
 }
